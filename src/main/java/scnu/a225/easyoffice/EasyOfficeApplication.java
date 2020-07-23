687c026ee28199164678771fc200b3fbe9f6b8e2
JiangJian1998 <1197749258@qq.com>
2020-07-23 21:26:52 +0800

    初始提交

---
+++ src/main/java/scnu/a225/easyoffice/EasyOfficeApplication.java
@@ -0,0 +1,13 @@
+package scnu.a225.easyoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scnu/a225/easyoffice/EasyOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package scnu.a225.easyoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
