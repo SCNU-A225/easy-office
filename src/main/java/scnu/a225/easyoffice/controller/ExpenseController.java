86a00af7a5346cef4800f88e16318a192f815221
Machinec <1585645621@qq.com>
2020-07-26 01:57:27 +0800

    ExpenseController

--- src/main/java/scnu/a225/easyoffice/controller/ExpenseController.java
+++ src/main/java/scnu/a225/easyoffice/controller/ExpenseController.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import scnu.a225.easyoffice.config.ShiroConfig;
 import scnu.a225.easyoffice.dao.ExpenseDao;
+import scnu.a225.easyoffice.utils.Result;
 
 import javax.servlet.http.HttpServletRequest;import javax.servlet.http.HttpSession;
+import java.io.ObjectInput;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,6 +43,98 @@ public class ExpenseController {
     public Object create(String formData, HttpServletRequest request) {
         JSONObject jsonObject = JSONObject.parseObject(formData);
         return "ok";
+    }
+
+    /**
+     * TODO
+     * 3.3提交报销单
+     */
+    @PostMapping("/submit")
+    public Object submitVoucher(HttpSession session, int id, double amount){
+        String deal_sn = (String) session.getAttribute("sn");
+        String department_sn = (String) session.getAttribute("department_sn");
+        String status = "已提交";
+        String deal_way = "提交";
+        String deal_result = "已提交";
+        String comment = "提交报销单";
+        String next_deal_sn = "部门经理";
+        if (amount < 5000) next_deal_sn = expenseDao.randEmployee(department_sn,"部门经理");
+        else next_deal_sn = expenseDao.randNextDeal("总经理");
+        expenseDao.updateVoucherStatus(next_deal_sn,status,id);
+        expenseDao.insertRecord(id,deal_sn,deal_way,deal_result,comment);
+        return new Result(200,"提交成功");
+    }
+
+    /**
+     * 3.4通过报销单
+     */
+    @PostMapping("/agree")
+    public Object agreeVoucher(HttpSession session, int id, String comment){
+        String deal_sn = (String) session.getAttribute("sn");
+        String status = "已通过";
+        String deal_way = "审核";
+        String deal_result = "通过";
+        String next_deal_sn = expenseDao.randNextDeal("财务");
+        expenseDao.updateVoucherStatus(next_deal_sn,status,id);
+        expenseDao.insertRecord(id,deal_sn,deal_way,deal_result,comment);
+        return new Result(200,"审核成功");
+    }
+
+
+    /**
+     * 3.5打回报销单
+     */
+    @PostMapping("/return")
+    public Object returnVoucher(HttpSession session, int id, String comment){
+        String deal_sn = (String) session.getAttribute("sn");
+        String status = "已打回";
+        String deal_way = "审核";
+        String deal_result = "打回";
+        String create_sn = expenseDao.selectVoucherOwner(id);
+        expenseDao.updateVoucherStatus(create_sn,status,id);
+        expenseDao.insertRecord(id,deal_sn,deal_way,deal_result,comment);
+        return new Result(200,"审核成功");
+    }
+
+    /**
+     * 3.6拒绝报销单
+     */
+    @PostMapping("/refuse")
+    public Object refuseVoucher(HttpSession session,int id, String comment){
+        String deal_sn = (String) session.getAttribute("sn");
+        String next_deal_sn = "00000";
+        String status = "已拒绝";
+        String deal_way = "审核";
+        String deal_result = "已拒绝";
+        expenseDao.updateVoucherStatus(next_deal_sn,status,id);
+        expenseDao.insertRecord(id,deal_sn,deal_way,deal_result,comment);
+        return new Result(200,"审核成功");
+    }
+
+    /**
+     * 3.7打款报销单
+     */
+    @PostMapping("/pay")
+    public Object payVoucher(HttpSession session,int id, String comment){
+        String deal_sn = (String) session.getAttribute("sn");
+        String next_deal_sn = "00000";
+        String status = "已打款";
+        String deal_way = "审核";
+        String deal_result = "已打款";
+        expenseDao.updateVoucherStatus(next_deal_sn,status,id);
+        expenseDao.insertRecord(id,deal_sn,deal_way,deal_result,comment);
+        return new Result(200,"审核成功");
+    }
+
+    /**
+     * 3.8查看待处理报销单
+     */
+    @GetMapping("todo")
+    public Map<String,Object> getTodo(HttpSession session){
+        Map<String,Object> result = new HashMap<>();
+        String sn = (String) session.getAttribute("sn");
+        result.put("arr",expenseDao.selectTodo(sn));
+        return result;
     }
 
     /**
--- src/main/java/scnu/a225/easyoffice/dao/ExpenseDao.java
+++ src/main/java/scnu/a225/easyoffice/dao/ExpenseDao.java
@@ -1,6 +1,8 @@
 package scnu.a225.easyoffice.dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -24,4 +26,29 @@ public interface ExpenseDao {
     // 获取个人所有报销单
     @Select("SELECT id, cause, create_time, total_amount, status FROM claim_voucher WHERE create_sn=#{sn}")
     List<Map<String,Object>> selectAllVoucher(String sn);
+
+    // 获取待处理报销单
+    @Select("SELECT c.id, c.cause, c.create_time, c.total_amount, c.status, e.name FROM claim_voucher c," +
+            "employee e WHERE c.next_deal_sn=#{sn} AND c.create_sn=e.sn")
+    List<Map<String,Object>> selectTodo(String sn);
+
+    // 修改报销单状态
+    @Update("update claim_voucher set next_deal_sn=#{next_deal_sn},status=#{status} WHERE id=#{claim_voucher_id}")
+    void updateVoucherStatus(String next_deal_sn,String status,int claim_voucher_id);
+
+    // 插入报销单处理记录
+    @Insert({"insert into deal_record values(null,#{claim_voucher_id},#{deal_sn},NOW(),#{deal_way},#{deal_result},#{comment})"})
+    void insertRecord(int claim_voucher_id,String deal_sn,String deal_way,String deal_result,String comment);
+
+    // 获取报销单的创建者
+    @Select("SELECT create_sn FROM claim_voucher WHERE id=#{id}")
+    String selectVoucherOwner(int id);
+
+    // 随机抽取一名某职位职员的sn
+    @Select("SELECT sn FROM employee WHERE post=#{post} order by rand() LIMIT 1")
+    String randNextDeal(String post);
+
+    // 随机抽取一名某部门的职员
+    @Select("SELECT sn FROM employee WHERE department_sn=#{department_sn} and post=#{post} order by rand() LIMIT 1")
+    String randEmployee(String department_sn, String post);
 }
