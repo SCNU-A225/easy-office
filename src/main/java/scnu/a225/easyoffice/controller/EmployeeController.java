2439c26f26596e8101083b107f4ceabca30ffc81
JiangJian1998 <1197749258@qq.com>
2020-07-25 22:56:39 +0800

    报销单

--- src/main/java/scnu/a225/easyoffice/controller/EmployeeController.java
+++ src/main/java/scnu/a225/easyoffice/controller/EmployeeController.java
@@ -10,13 +10,12 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import scnu.a225.easyoffice.dao.EmployeeDao;
 import scnu.a225.easyoffice.entity.Employee;
+import scnu.a225.easyoffice.utils.Result;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
@@ -38,7 +37,10 @@ public class EmployeeController {
     private EmployeeDao employeeDao;
 
     @PostMapping("/user/login")
-    public Map<String, Object> login(String sn, String password) {
+    public Object login(String sn, String password) {
+        if(null == sn || sn.isEmpty()) return new Result(401, "请输入工号");
+        else if(null == password || password.isEmpty()) return new Result(401, "请输入密码");
+
         Subject subject = SecurityUtils.getSubject();   //获取当前用户
         UsernamePasswordToken token = new UsernamePasswordToken(sn, password);    //封装令牌
         try {
@@ -53,18 +55,23 @@ public class EmployeeController {
                 put("department", employeeInfo.get("department_name"));
             }};
         } catch (UnknownAccountException e) {
-            return new HashMap<String, Object>() {{
-                put("msg", "用户名或密码不正确");
-            }};
+            return new Result(403, "工号或密码不正确");
         } catch (IncorrectCredentialsException e) {
-            return new HashMap<String, Object>() {{
-                put("msg", "IncorrectCredentialsException");
-            }};
-        } catch (AuthenticationException e) {
+            return new Result(404, "IncorrectCredentialsException");
+        } catch (Exception e) {
             e.printStackTrace();
+            return new Result(405, "未知错误");
         }
-
-        return null;
     }
+
+    @GetMapping("/logout")
+    public Object logout(HttpSession session) {
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();   //shiro注销
+        return new Result(200, "注销成功");
+    }
+
+
+
 
 }
