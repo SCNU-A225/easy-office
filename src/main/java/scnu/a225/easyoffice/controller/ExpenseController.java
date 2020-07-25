a1b0a956c5fc82dfcf3a72012af923c6e22fcbd2
JiangJian1998 <1197749258@qq.com>
2020-07-25 23:32:28 +0800

    完成基本信息api

--- src/main/java/scnu/a225/easyoffice/controller/ExpenseController.java
+++ src/main/java/scnu/a225/easyoffice/controller/ExpenseController.java
@@ -1,8 +1,12 @@
 package scnu.a225.easyoffice.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName: ExpenseController
- * @Description: TODO
+ * @Description: 报销单Conrtoller
  * @Author: jiangjian
  * @CreateDate: 2020/7/25 22:56
  * @UpdateUser: jiangjian
@@ -10,5 +14,12 @@ package scnu.a225.easyoffice.controller;
  * @UpdateRemark: TODO
  * @Version: V1.0
  */
+@RestController
+@RequestMapping("/expense")
 public class ExpenseController {
+    @PostMapping
+    public Object create() {
+        return null;
+    }
+
 }
--- src/main/java/scnu/a225/easyoffice/dao/EmployeeDao.java
+++ src/main/java/scnu/a225/easyoffice/dao/EmployeeDao.java
@@ -1,6 +1,8 @@
 package scnu.a225.easyoffice.dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Component;
 import scnu.a225.easyoffice.entity.Employee;
 
@@ -14,4 +16,8 @@ public interface EmployeeDao {
     @Select("SELECT employee.sn,password,employee.name,department.name as department_name,department.sn as department_sn,post FROM employee,department " +
             "WHERE employee.sn=#{sn} and password=#{password} and employee.department_sn=department.sn")
     Map<String, Object> getEmployeeInfo(String sn, String password);
+
+    @Update("UPDATE employee SET password=#{newPassword} WHERE sn=#{sn} and password=#{password}")
+    int changePassword(String password, String newPassword, String sn);
+
 }
